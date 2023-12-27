820904f8eec4432d58160f46cfabb8907bfbddf1
zhengzhoule <1453250595@qq.com>
2023-12-27 17:59:04 +0800

    fix: to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>home</div>;
+  const router = useRouter()
+  useEffect(() => {
+    router.replace('/home')
+  },[router])
+  return null;
 }
