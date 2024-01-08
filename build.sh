5d9798203be3d3cac3db614befa53b600624af3e
fuzhenyuan <fuzhenyuan@zego.im>
2024-01-08 18:22:27 +0800

    chore: add build.sh to git user's docs

---
+++ build.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+# 取环境变量
+echo $token_3th
+echo $username
+echo $repo
+
+#  安装环境依赖
+# build build test
+
+echo "start delete template docs"
+rm -rf docs
+echo "delete template docs end"
+
+# 克隆远端用户 docs 仓库，拷贝到指定目录，推送到远端
+git clone "https://$username:$token_3th@github.com/$username/$repo.git" ./docs
