cdaa3b20440fc2b919ed973af2be3e8b05b82a35
tilince <tilince@aliyun.com>
2024-01-12 19:23:06 +0800

    Update favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,25 +1,29 @@
 import Head from "next/head";
 import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
+import LibControllerImpl from "@/lib/index";
 
 const Meta = () => {
+  const docuoConfig = LibControllerImpl.getDocuoConfig();
+  const favicon = `/${docuoConfig.favicon}` || "/favicon/favicon.ico";
+  console.log(`Meta favicon`, favicon);
   return (
-    <Head>
+    <>
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href={favicon || "/favicon/apple-touch-icon.png"}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href={favicon || "/favicon/favicon-32x32.png"}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href={favicon || "/favicon/favicon-16x16.png"}
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
@@ -27,7 +31,7 @@ const Meta = () => {
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="shortcut icon" href={favicon} />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
@@ -37,7 +41,7 @@ const Meta = () => {
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-    </Head>
+    </>
   );
 };
 
