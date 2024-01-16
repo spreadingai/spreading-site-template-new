8da2735ec9914cf8a4dd94807a8b07b817dbdf42
fuzhenyuan <fuzhenyuan@zego.im>
2024-01-16 18:30:22 +0800

    fix(3002595531): 新标签页打开http链接

--- plugins/rehype/rehypeLink.ts
+++ plugins/rehype/rehypeLink.ts
@@ -10,31 +10,32 @@ export function rehypeLink(options: {
 }) {
   return function updateLinkTag(tree, file) {
     visit(tree, "element", (node, i, parent) => {
+      if (node.tagName !== "a" || !node.properties.href) return;
+      // http 链接在新标签打开
+      if (node.properties.href.startsWith("http")) {
+        node.properties.target = "_blank";
+        return;
+      }
       //   console.log("rehypeLink ", node.tagName, node.properties);
       if (!options.rootUrl || !options.filePath) return;
-      if (
-        node.tagName === "a" &&
-        node.properties.href &&
-        !node.properties.href.startsWith("http")
-      ) {
-        const href = node.properties.href;
-        const parsedPath = path.parse(href);
-        const targetHref = `${parsedPath.dir}/${parsedPath.name}`;
-        const imagePath = path.resolve(
-          path.dirname(options.filePath),
-          targetHref
-        );
-        const publicPath = path.relative(options.rootUrl, imagePath);
-        console.log(
-          `[rehypeLink]updateLinkTag`,
-          options.prefix,
-          options.rootUrl,
-          imagePath,
-          targetHref,
-          publicPath
-        );
-        node.properties.href = `${options.prefix}/${publicPath}`;
-      }
+      // while href does not start with 'http'
+      const href = node.properties.href;
+      const parsedPath = path.parse(href);
+      const targetHref = `${parsedPath.dir}/${parsedPath.name}`;
+      const imagePath = path.resolve(
+        path.dirname(options.filePath),
+        targetHref
+      );
+      const publicPath = path.relative(options.rootUrl, imagePath);
+      console.log(
+        `[rehypeLink]updateLinkTag`,
+        options.prefix,
+        options.rootUrl,
+        imagePath,
+        targetHref,
+        publicPath
+      );
+      node.properties.href = `${options.prefix}/${publicPath}`;
     });
   };
 }
