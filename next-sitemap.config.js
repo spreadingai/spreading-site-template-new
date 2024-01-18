e670ddb70384df0e23835480dd9a5449c4146999
tilince <tilince@aliyun.com>
2024-01-18 16:56:58 +0800

    Build

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL,
+  exclude: ["/home"],
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
