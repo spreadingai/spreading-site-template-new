0e8dd65fd3e52436999498923d7164dd927cd0ae
tilince <tilince@aliyun.com>
2024-01-31 22:31:47 +0800

    Temp

--- packages/docusaurus-theme-openapi-docs/lib/theme/ApiItem/index.d.ts
+++ packages/docusaurus-theme-openapi-docs/lib/theme/ApiItem/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-/// <reference types=".pnpm/@docusaurus+plugin-content-docs@3.1.1_react-dom@18.2.0_react@18.2.0_typescript@5.3.3/node_modules/@docusaurus/plugin-content-docs" />
-/// <reference types=".pnpm/@docusaurus+plugin-content-docs@2.4.3_react-dom@18.2.0_react@18.2.0_typescript@5.3.3/node_modules/@docusaurus/plugin-content-docs" />
+/// <reference types=".pnpm/@docusaurus+plugin-content-docs@3.1.1_eslint@8.56.0_react-dom@18.2.0_react@18.2.0_typescript@5.3.3/node_modules/@docusaurus/plugin-content-docs" />
+/// <reference types=".pnpm/@docusaurus+plugin-content-docs@2.4.3_eslint@8.56.0_react-dom@18.2.0_react@18.2.0_typescript@5.3.3/node_modules/@docusaurus/plugin-content-docs" />
 import type { Props } from "@theme/DocItem";
 export default function ApiItem(props: Props): JSX.Element;
