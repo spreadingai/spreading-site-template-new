115ec1ef1819a4e2369158c1fdbb8f8b3a823aac
tilince <tilince@aliyun.com>
2024-02-18 14:22:21 +0800

    Update root

--- components/docuoOpenapi/theme/ApiItem/index.tsx
+++ components/docuoOpenapi/theme/ApiItem/index.tsx
@@ -7,8 +7,6 @@
 
 import zlib from "zlib";
 import React from "react";
-import clsx from "clsx";
-import useIsBrowser from "../../core/client/exports/useIsBrowser";
 import { ServerObject } from "docusaurus-plugin-openapi-docs/src/openapi/types";
 import { ParameterObject } from "docusaurus-plugin-openapi-docs/src/openapi/types";
 import type { ApiItem as ApiItemType } from "docusaurus-plugin-openapi-docs/src/types";
@@ -39,66 +37,64 @@ export default function ApiItem(props: Props): JSX.Element {
       zlib.inflateSync(Buffer.from(api as any, "base64")).toString()
     );
     console.log("####api", api);
-  }
 
-  const isBrowser = true;
+    const isBrowser = true;
 
-  // Regex for 2XX status
-  const statusRegex = new RegExp("(20[0-9]|2[1-9][0-9])");
+    // Regex for 2XX status
+    const statusRegex = new RegExp("(20[0-9]|2[1-9][0-9])");
 
-  // Define store2
-  let store2: any = {};
+    // Define store2
+    let store2: any = {};
 
-  // Init store for SSR
-  if (!isBrowser) {
-    store2 = createStoreWithoutState({}, []);
-  }
-
-  // Init store for CSR to hydrate components
-  if (isBrowser) {
-    // Create list of only 2XX response content types to create request samples from
-    let acceptArray: any = [];
-    for (const [code, content] of Object.entries(api?.responses ?? [])) {
-      if (statusRegex.test(code)) {
-        acceptArray.push(Object.keys(content.content ?? {}));
-      }
+    // Init store for SSR
+    if (!isBrowser) {
+      store2 = createStoreWithoutState({}, []);
     }
-    acceptArray = acceptArray.flat();
 
-    const content = api?.requestBody?.content ?? {};
-    const servers = api?.servers ?? [];
-    const params = {
-      path: [] as ParameterObject[],
-      query: [] as ParameterObject[],
-      header: [] as ParameterObject[],
-      cookie: [] as ParameterObject[],
-    };
-    api?.parameters?.forEach(
-      (param: { in: "path" | "query" | "header" | "cookie" }) => {
-        const paramType = param.in;
-        const paramsArray: ParameterObject[] = params[paramType];
-        paramsArray.push(param as ParameterObject);
+    // Init store for CSR to hydrate components
+    if (isBrowser) {
+      // Create list of only 2XX response content types to create request samples from
+      let acceptArray: any = [];
+      for (const [code, content] of Object.entries(api?.responses ?? [])) {
+        if (statusRegex.test(code)) {
+          acceptArray.push(Object.keys(content.content ?? {}));
+        }
       }
-    );
-    // TODO: determine way to rehydrate without flashing
-    // const acceptValue = window?.sessionStorage.getItem("accept");
-    // const contentTypeValue = window?.sessionStorage.getItem("contentType");
-    // const server = window?.sessionStorage.getItem("server");
-    const server = JSON.stringify(api.servers[0]);
-    const serverObject = (JSON.parse(server!) as ServerObject) ?? {};
+      acceptArray = acceptArray.flat();
 
-    store2 = createStoreWithState(
-      {
-        server: {
-          value: (serverObject as any).url ? serverObject : undefined,
-          options: servers,
+      const content = api?.requestBody?.content ?? {};
+      const servers = api?.servers ?? [];
+      const params = {
+        path: [] as ParameterObject[],
+        query: [] as ParameterObject[],
+        header: [] as ParameterObject[],
+        cookie: [] as ParameterObject[],
+      };
+      api?.parameters?.forEach(
+        (param: { in: "path" | "query" | "header" | "cookie" }) => {
+          const paramType = param.in;
+          const paramsArray: ParameterObject[] = params[paramType];
+          paramsArray.push(param as ParameterObject);
+        }
+      );
+      // TODO: determine way to rehydrate without flashing
+      // const acceptValue = window?.sessionStorage.getItem("accept");
+      // const contentTypeValue = window?.sessionStorage.getItem("contentType");
+      // const server = window?.sessionStorage.getItem("server");
+      const server = JSON.stringify(api.servers[0]);
+      const serverObject = (JSON.parse(server!) as ServerObject) ?? {};
+
+      store2 = createStoreWithState(
+        {
+          server: {
+            value: (serverObject as any).url ? serverObject : undefined,
+            options: servers,
+          },
         },
-      },
-      []
-    );
-  }
-
-  if (api) {
+        []
+      );
+    }
     return <Provider store={store2}>{children}</Provider>;
   }
+  return <>{children}</>;
 }
