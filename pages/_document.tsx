6382f68d56ab49181ec21454d03c44ac5bc9f62f
lihao <lihao@zego.im>
2025-01-10 10:53:47 +0800

    Support baidu tongji

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import CommonMeta from "@/components/meta/CommonMeta";
+import Script from "next/script";
+import LibControllerImpl from "@/lib/index";
 
 export default function Document() {
+  const docuoConfig = LibControllerImpl.getDocuoConfig();
+  const { analytics = {} } = docuoConfig;
+  const baiduTongjiStr = analytics["baidu-tongji"];
   return (
     <Html lang="en">
       <Head>
@@ -10,6 +15,15 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {baiduTongjiStr ? (
+          <Script
+            id="baidu-tongji"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: baiduTongjiStr,
+            }}
+          />
+        ) : null}
       </body>
     </Html>
   );
