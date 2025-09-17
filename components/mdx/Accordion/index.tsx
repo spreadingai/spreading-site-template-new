02814e0194317787f3f8ddc412c9c26691f4a413
oliver <yangwanqing.me@qq.com>
2025-09-17 17:34:51 +0800

    修复Accordion箭头旋转错误

--- components/mdx/Accordion/index.tsx
+++ components/mdx/Accordion/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./index.module.scss";
 const IconExpandablePackup = () => (
   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-    <polyline points="6 9 12 15 18 9"></polyline>
+    <polyline points="9 18 15 12 9 6"></polyline>
   </svg>
 );
 
