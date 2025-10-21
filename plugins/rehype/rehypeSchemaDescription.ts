c60d098446fe14f4af8103cb0ff89cd2893abad2
lihao <lihao@zego.im>
2025-10-21 10:40:40 +0800

    temp

--- plugins/rehype/rehypeSchemaDescription.ts
+++ plugins/rehype/rehypeSchemaDescription.ts
@@ -32,8 +32,56 @@ export function rehypeSchemaDescription() {
       if (targetAttribute.value.value !== `"schema-description"`){
         return;
       }
-
       // 3. 获取文本内容
+      if (parent.children.length === 1) {
+        // 1.有引用（纯文本）：'ZIM 相关信息。>> - 仅当 MessageHistory.SyncMode 为 0 时有效。> - 请确保您的项目已开通 ZIM 服务。> - 请确保已调用 ZIM 机器人注册接口，并将返回的 UserInfo.UserId 作为 RobotId。> - 建议您提前注册机器人，以便完善用户信息设置并提升智能体实例的创建效率。'
+        // 2.纯列表：'从 LLM 返回的内容中过滤指定标点符号内的文本，然后再进行语音合成。注意：- 需要在 LLM \> SystemPrompt 中定义哪些内容应该放在指定标点符号内- 此参数在更新智能体实例时无法更新'
+        // 注意：普通 >（非引用）一定要用反斜杠 \ 转义
+      } else {
+        // 3.有引用（有子元素）children
+        // [
+        //   {
+        //     type: "text",
+        //     value: "ZIM 相关信息。> ",
+        //   },
+        //   {
+        //     type: "strong",
+        //     children: [
+        //       {
+        //         type: "text",
+        //         value: "📌 重要说明",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     type: "text",
+        //     value: ">> - 仅当 MessageHistory.SyncMode 为 0 时有效。> - 请确保您的项目已开通 ZIM 服务。> - 请确保已调用 ZIM 机器人注册接口，并将返回的 UserInfo.UserId 作为 RobotId。> - 建议您提前注册机器人，以便完善用户信息设置并提升智能体实例的创建效率。",
+        //   },
+        // ]
+        // 4.有引用（有子元素）children
+        // [
+        //   {
+        //     type: "text",
+        //     value: "ZIM 相关信息。> ",
+        //   },
+        //   {
+        //     type: "mdxJsxTextElement",
+        //     name: "strong",
+        //     attributes: [
+        //     ],
+        //     children: [
+        //       {
+        //         type: "text",
+        //         value: "📌 重要说明",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     type: "text",
+        //     value: ">> - 仅当 MessageHistory.SyncMode 为 0 时有效。> - 请确保您的项目已开通 ZIM 服务。> - 请确保已调用 ZIM 机器人注册接口，并将返回的 UserInfo.UserId 作为 RobotId。> - 建议您提前注册机器人，以便完善用户信息设置并提升智能体实例的创建效率。",
+        //   },
+        // ]
+      }
       const fullText = node.value;
 
       // 4. 用正则表达式提取所有 "- " 开头的列表项
