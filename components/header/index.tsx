26ab7882caf6eaf8a6769a5de5ddd7fe6d8cb1de
oliver <yangwanqing.me@qq.com>
2026-04-02 15:54:17 +0800

    页头页脚优化

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -166,6 +166,16 @@ const Header = (props: Props) => {
     ) : null;
   };
 
+  const devCenterNav = useMemo(() => {
+    return currentLanguage === "zh"
+      ? [
+          { label: "SDK 中心", href: "https://doc-zh.zego.im/sdk-download/2968" },
+          { label: "API 中心", href: "https://doc-zh.zego.im/api-center" },
+          { label: "常见问题", href: "https://doc-zh.zego.im/faq/overview" },
+        ]
+      : [];
+  }, [currentLanguage]);
+
   const renderLanguageSwitch = () => {
     return !!displayLanguages?.length ? (
       !isSearchPage ? (
@@ -185,7 +195,9 @@ const Header = (props: Props) => {
         scrollLength === 0 ? styles["header-bg-opacity"] : styles["header-bg"]
       } ${isSearchPage ? styles["search-page"] : ""}`}
     >
-      <div className={`container-wrap ${styles.container}`}>
+      <div
+        className={`container-wrap ${styles.container} ${currentLanguage === "zh" ? styles.zh : ""}`}
+      >
         <div className="flex items-center">
           {logo ? (
             <div className="flex items-center">
@@ -212,6 +224,17 @@ const Header = (props: Props) => {
               </a>
             </div>
           ) : null}
+          <div className={styles["old-dev-center"]}>
+            <ul className={styles["old-dev-center-nav"]}>
+              {devCenterNav.map((item, index) => (
+                <li key={index} className={styles["old-dev-center-nav-item"]}>
+                  <Link href={item.href || "/"}>{item.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className={styles["fixed-menus"]}>
           {!isSearchPage ? DocSearchComponent : null}
           {!isSearchPage && themeConfig.showAskAI !== false ? (
             <AISearch />
@@ -221,7 +244,9 @@ const Header = (props: Props) => {
           <div className={styles["menus"]}>
             <Mobile
               // @ts-ignore
-              menus={(items || []).map((item) => {
+              menus={([...(navbar.title ? [
+                { label: navbar.title, href: navbar.iconRedirectUrl },
+              ] : []), ...devCenterNav, ...items] || []).map((item) => {
                 if (item.label) {
                   return item;
                 }
